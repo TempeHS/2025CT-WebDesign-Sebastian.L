7f90cb62c5b02b6d4bbc50b538b51fd5cdd12d1f
Sebastian Lam <sebastian.lam1@DETNSW>
2024-03-11 14:14:16 +1100

    start laying out game
    Hello, name and number prompt

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let MyAlert = 5;
-let enterName = 5;
-
-if(MyAlert == enterName)
+let enterName = prompt("Enter your name");
+loadDIV ();
 
 
-alert(MyAlert);
+function loadDIV (){
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1><H2>Guess a number between 1&2</H2>";
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "</H1><H2>Guess a number between 1&2</H2>";
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback;
+}
+
+function enterNumber () {
+    PlayerGuess = prompt("Enter a number")
+}
